a52e2484b17638b013457379c756db32bb075d14
anaparastasi <137augusto@gmail.com>
2022-07-30 19:20:27 -0300

    atualização do curso de js

--- aula6 (ex. hora do dia)/modelo/script.js
+++ aula6 (ex. hora do dia)/modelo/script.js
@@ -5,20 +5,18 @@ function carregar() {
     var msg = window.document.getElementById("msg")
     var img = window.document.getElementById("imagem")
     var data = new Date()
-    var hora = data.getHours()
-    
-    
-    msg.innerHTML = `Agora são ${hora} horas.`
+    //var hora = data.getHours()
+    var hora =  15
     if (hora >= 0 && hora <= 12) {
-        //BOM DIA
-        img.src= 'imagens/manhã.png'
+        msg.innerHTML =  `Agora são ${hora} horas. <br> Bom Dia`
+        img.src= 'imagens/manha.png'
         document.body.style.background = '#f5cd60e8'
     } else if (hora >= 12 && hora <= 18){
-        //BOA TARDE
-        img.src= 'imagens/dia.png'
+        msg.innerHTML =  `Agora são ${hora} horas. <br> Boa Tarde`
+        img.src= 'imagens/tarde.png'
         document.body.style.background = '#ad401fe8'
     } else {
-        //BOA NOITE
+        msg.innerHTML =  `Agora são ${hora} horas. <br> Boa Noite`    
         img.src= 'imagens/noite.png'
         document.body.style.background = '#0f2970da'
     }
