c2835017b3d267bc2f1637d86562a8d3d44ae625
anaparastasi <137augusto@gmail.com>
2022-07-30 22:21:25 -0300

    atualização do curso de js

---
+++ aula7 (ex. idade)/modelo/script.js
@@ -0,0 +1,47 @@
+function verificar() {
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res = document.getElementById('res')
+   if (fano.value.lenght == 0 && fano.value > ano){
+    window.alert('Verifique os dados e tente novamente.')
+   } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ``
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked){
+            gênero = 'Homem'
+            if (idade >=0 && idade<10){
+                img.setAttribute('src' , 'bebe.png')
+
+            } else if ( idade < 25) {
+                img.setAttribute('src', 'menino.png')
+            } else if (idade <60) {
+                img.setAttribute('src' , 'homem.png')
+            } else{
+                img.setAttribute('src' , 'idoso.png')
+            }
+
+            
+        } else if (fsex[1].checked){
+            gênero = 'Mulher'
+            if (idade >=0 && idade<10){
+                img.setAttribute('src' , 'bebe.png')
+            } else if ( idade < 25) {
+                img.setAttribute('src' , 'jovem-mulher.png')
+            } else if (idade <60) {
+                img.setAttribute('src' , 'mulher.png')
+            } else {
+                img.setAttribute('src' , 'idosa.png')
+            }
+
+        }
+        res.style. textAlign = 'Center'
+        res.style.margin = '10px'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+   }
+}
